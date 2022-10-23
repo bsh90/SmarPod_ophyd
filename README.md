7ffd69e2c968423d67a8da53ab0944971478bd69
bsh <bshidrang@yahoo.com>
2022-10-24 00:20:58 +0200

    Change the test to directly use SmarPod class method

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# SmarPod_ophyd
+# SmarPod Ophyd Interface
+
+With this application it's possible to connect to a SmarPod, either via Ophyd interface or directly to its API.
 
 This application is made as part of a master thesis in Helmholtz Zentrum Berlin.
 
 Author: Bahareh Shidrang
 
-Code Reviewer: William Smith
+Date: October 2022
+
+Institute: Helmholtz Zentrum Berlin, Department Interface Design.
+
+In case of using this library in any publication, please cite the author. Thank you.
--- TestSetSmarPod.py
+++ TestSetSmarPod.py
@@ -7,49 +7,46 @@ import time
 
 class TestSmarPod(unittest.TestCase):
 
-    def set(self, pose, handle, smarpod_object):
-        self.verify_reachable_position(pose, handle)
+    # def set(self, pose, handle, smarpod_object):
+    #     self.verify_reachable_position(pose, handle)
 
-        print("moving to %s" % smarpod_object.pose_to_str(pose))
-        smarpod.Move(handle, pose, 0, True)
+    #     print("moving to %s" % smarpod_object.pose_to_str(pose))
+    #     smarpod.Move(handle, pose, 0, True)
 
-        time.sleep(1)
-        self.verify_stopped_status(handle)
-        return self.get_status(handle)
+    #     time.sleep(1)
+    #     self.verify_stopped_status(handle)
+    #     return self.get_status(handle)
 
-    def get_status(self, handle):
-        move_status = smarpod.GetMoveStatus(handle)
-        if (move_status == smarpod.MoveStatus.STOPPED):
-            return True
-        else:
-            return False
+    # def get_status(self, handle):
+    #     move_status = smarpod.GetMoveStatus(handle)
+    #     if (move_status == smarpod.MoveStatus.STOPPED):
+    #         return True
+    #     else:
+    #         return False
 
-    def verify_stopped_status(self, handle):
-        if not self.get_status(handle):
-            error = Exception("Status can not be stopped.")
-            raise error
+    # def verify_stopped_status(self, handle):
+    #     if not self.get_status(handle):
+    #         error = Exception("Status can not be stopped.")
+    #         raise error
 
-    def check_pose(self, pose, handle):
-        if not smarpod.IsPoseReachable(handle, pose):
-            return False
-        else:
-            return True
+    # def check_pose(self, pose, handle):
+    #     if not smarpod.IsPoseReachable(handle, pose):
+    #         return False
+    #     else:
+    #         return True
 
-    def verify_reachable_position(self, pose, handle):
-        if not self.check_pose(pose, handle):
-            raise Exception("Not all poses in sequence are reachable")
+    # def verify_reachable_position(self, pose, handle):
+    #     if not self.check_pose(pose, handle):
+    #         raise Exception("Not all poses in sequence are reachable")
 
 
     def load_position_data_sample(self):
         pose_Home = Pose(0, 0, 0, 0, 0, 0)
         pose_sequence = [
-            pose_Home,
-            Pose(0, 0, -0.001, 0, 0, 5),
-            Pose(0, 0, -0.001, 0, 0, 7),
-            Pose(0, 0, -0.001, 0, 0, 9),
-            Pose(0, 0, -0.001, 0, 0, 11),
-            Pose(0, 0, -0.001, 0, 0, 13),
-            Pose(0, 0, -0.001, 0, 0, 14),
+            pose_Home ,
+            Pose(-0.0006, 0, 0, 0, -8, 14.2),
+            Pose(-0.0006, 0, 0, 0, -9, 14.2),
+            Pose(-0.0006, 0, 0, 0, -9.5, 14.2),
         ]
         return pose_sequence
 
@@ -58,11 +55,14 @@ class TestSmarPod(unittest.TestCase):
         smarpod_object = SmarPod()
         handle = smarpod_object.set_up()
 
-        for pose in pose_seq: 
-            status = self.set(pose, handle, smarpod_object)
-            newPose = smarpod.GetPose(handle)
-            print(smarpod_object.pose_to_double_list(newPose))
+        # for pose in pose_seq: 
+        #     status = self.set(pose, handle, smarpod_object)
+        #     newPose = smarpod.GetPose(handle)
+        #     print(smarpod_object.pose_to_double_list(newPose))
+        #     time.sleep(10)
 
+        newPose = smarpod_object.moving(handle, pose_seq)
+        print(smarpod_object.pose_to_str(newPose))
         smarpod_object.tear_down(handle)
 
 
